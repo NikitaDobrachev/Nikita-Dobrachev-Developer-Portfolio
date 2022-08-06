9abbc6b88ab84788402018a461b009cb42c1c026
NikitaDobrachev <102922035+NikitaDobrachev@users.noreply.github.com>
2022-08-06 20:30:01 -0300

    Delete Frontend Mentor Tasks folder
    Delete Frontend Mentor Tasks folder from DoctorCare branch to keep only DoctorCare web-app in this branch

--- Frontend Mentor Tasks (Frontend)/Interactive Rating Component/js/script.js
+++
@@ -1,5 +0,0 @@
-const rateAddress = window.location.href;
-
-const rate = rateAddress[rateAddress.length - 13];
-
-document.getElementById('rateMessageText').innerHTML = `You selected ${rate} out of 5`;
