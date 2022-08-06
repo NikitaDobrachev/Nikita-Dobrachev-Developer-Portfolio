31acf416e6cc7b927fc90f02ddc5acaea0fcd0e4
Nikita Dobrachev <nickdobrachev@gmail.com>
2022-08-05 21:18:10 -0300

    5 aug 2022

--- NLW/main.js
+++ NLW/main.js
@@ -1,15 +1,49 @@
+window.addEventListener('scroll', onScroll);
+
+onScroll();
+
 function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
+
+function showNavOnScroll() {
   if (window.scrollY > 0) {
     document.getElementById('navigation').classList.add('scroll')
   } else {
-    document.getElementById('navigation').classList.remove('scroll')
+    document.getElementById('navigation').classList.remove('scroll');
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (window.scrollY > 550) {
+    document.getElementById('backToTopButton').classList.add('show')
+  } else {
+    document.getElementById('backToTopButton').classList.remove('show');
   }
 }
 
 function openMenu() {
-  document.body.classList.add('menu-expanded')
+  document.body.classList.add('menu-expanded');
 }
 
 function closeMenu() {
   document.body.classList.remove('menu-expanded');
 }
+
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services header,
+  #services .card,
+  #about,
+  #about header,
+  #about .content,
+  #contact,
+  #contact header,
+  #contact .content`);
